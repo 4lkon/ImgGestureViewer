5d62a1e563831bcdfdf421a49d980cc2c2ae0554
4lkon <alkon612@gmail.com>
2019-02-23 14:30:22 +0100

    v1
    v1

---
+++ ImgGestureViewer/HsvSkinDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//emgu
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImgGestureViewer.SkinDetector
+{
+    class HsvSkinDetector : IColorSkinDetector
+    {
+        public override Image<Gray, byte> DetectSkin(Image<Bgr, byte> Img, IColor min, IColor max)
+        {
+            Image<Hsv, Byte> currentHsvFrame = Img.Convert<Hsv, Byte>();
+            Image<Gray, byte> skin = new Image<Gray, byte>(Img.Width, Img.Height);
+            skin = currentHsvFrame.InRange((Hsv)min, (Hsv)max);
+            return skin;
+        }
+    }
+}
---
+++ ImgGestureViewer/IColorSkinDetector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//emgu
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ImgGestureViewer.SkinDetector
+{
+    public abstract class IColorSkinDetector
+    {
+        public abstract Image<Gray, Byte> DetectSkin(Image<Bgr, Byte> Img, IColor min, IColor max);
+    }
+}
